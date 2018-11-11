ecbfc2851b619113414f385eb419774856c2ed45
Marc Poppy <poppy.marc@gmail.com>
2018-11-11 05:53:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,11 +6,10 @@ def valid_move?(board, index)
   valid = nil
   if position_taken?(board, index) == true #this meaning that position is open
     #need to execute to see if the position is valid now (meaning that its between 0-8)
-    valid = index.between?(0,8)
-    if valid == true
-      return valid
-    else valid == false
-      return valid
+    if valid = index.between?(0,8)
+      true
+    else
+      false
     end
 else position_taken?(board, index) == false #meaning the position is taken, needs to return false
     return false
