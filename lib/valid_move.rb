01a6d915fa3f0baf66cd2cf5303063a007121c9c
Marc Poppy <poppy.marc@gmail.com>
2018-11-11 05:23:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken(board, index)
+  if position_taken(board, index) == false #this meaning that position is open
 
 
   end
@@ -11,6 +11,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
+#this method defines is the position is taken or not 
 def position_taken?(board,index)
   taken = nil
   if board[input] == " " || board[input] = "" || board[index] == nil
