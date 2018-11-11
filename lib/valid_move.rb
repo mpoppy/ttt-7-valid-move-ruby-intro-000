59e14c6bf206c25cc3da0731f03a518f30ebb7ca
Marc Poppy <poppy.marc@gmail.com>
2018-11-11 05:22:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,18 +2,20 @@
 
 def valid_move?(board, index)
   if position_taken(board, index)
-    
+
+
+  end
 end
 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken(board,index)
+def position_taken?(board,index)
   taken = nil
   if board[input] == " " || board[input] = "" || board[index] == nil
-    taken = false
+    taken = false #this would mean spot is open, position is not taken
   else
-    taken = true
+    taken = true #this would mean that position is taken
   end
 end
