1003de2514eeba68321c5ff4af4b6557d369c186
Marc Poppy <poppy.marc@gmail.com>
2018-11-11 06:02:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,6 +7,7 @@ def valid_move?(board, index)
   if position_taken?(board, index) == true #this meaning that position is open
     #need to execute to see if the position is valid now (meaning that its between 0-8)
     if index.between?(0,8)
+      true
     end
   else
     return
